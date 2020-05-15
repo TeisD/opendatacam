50e1a8a87a6a0641bfce8f09cf86d060bef3998e
Teis <teis@teis.me>
2020-05-15 15:21:26 +0200

    Clean up run script

--- platforms/nano/run.sh
+++ platforms/nano/run.sh
@@ -1,3 +1,22 @@
 #!/bin/bash
 
-echo "This is the run file for platform: nano"
+echo "Starting opendatacam on platform: nano"
+
+# Mount the configuration file
+DOCKER_VOLUMES+="-v $(pwd)/config.json:/var/local/opendatacam/config.json "
+# Mount a video directory for people to run on files
+DOCKER_VOLUMES+="-v $(pwd)/data/videos:/var/local/darknet/videos:ro "
+# Mount the directory containing neural network weights
+DOCKER_VOLUMES+="-v $(pwd)/data/weights:/var/local/darknet/weights:ro "
+# Mount the directory containing the database
+DOCKER_VOLUMES+="-v $(pwd)/data/db:/data/db "
+# Mount argus socket for CSI cam access
+DOCKER_VOLUMES+="-v /tmp/argus_socket:/tmp/argus_socket "
+shift
+# We use --priviliged here because usb cam access cam be /dev/video0 or /dev/video1 or something else
+# If you don't want to use --priviliged, you will need to manually mount the right --device in order for
+# the docker container to be able to access it.. For example:
+# --device=/dev/video0:/dev/video0
+# We don't do that by default because if the device isn't mounted on this location, docker run will fail
+# and we didn't find a way yet to be smart about this and guess which device to mount
+sudo docker run -d --name opendatacam --restart unless-stopped --runtime nvidia -p 8080:8080 -p 8090:8090 -p 8070:8070 --privileged $DOCKER_VOLUMES $@ opendatacam/opendatacam:OPENDATACAM_VERSION-nano
--- platforms/nvidiadocker/run.sh
+++ platforms/nvidiadocker/run.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 
-echo "This is the run file for platform: nvidia-docker"
+echo "Starting opendatacam on platform: nvidia-docker"
+
+# Mount the configuration file
+DOCKER_VOLUMES+="-v $(pwd)/config.json:/var/local/opendatacam/config.json "
+# Mount a video directory for people to run on files
+DOCKER_VOLUMES+="-v $(pwd)/data/videos:/var/local/darknet/videos:ro "
+# Mount the directory containing neural network weights
+DOCKER_VOLUMES+="-v $(pwd)/data/weights:/var/local/darknet/weights:ro "
+# Mount the directory containing the database
+DOCKER_VOLUMES+="-v $(pwd)/data/db:/data/db "
+shift
+# If usbcam add --device=/dev/video0:/dev/video0 ... Will improve to detect and add it auto from config.json
+docker run -d --name opendatacam --restart unless-stopped --gpus=all -p 8080:8080 -p 8090:8090 -p 8070:8070 $DOCKER_VOLUMES $@ opendatacam/opendatacam:OPENDATACAM_VERSION-nvidiadocker
