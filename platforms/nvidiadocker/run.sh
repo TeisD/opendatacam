74f00e82925e938739f2eba6d3d0d058fa0dbe0b
Teis <teis@teis.me>
2020-06-08 12:28:32 +0200

    Run with development image

--- platforms/nvidiadocker/run.sh
+++ platforms/nvidiadocker/run.sh
@@ -12,4 +12,7 @@ DOCKER_VOLUMES+="-v $(pwd)/data/weights:/var/local/darknet/weights:ro "
 DOCKER_VOLUMES+="-v $(pwd)/data/db:/data/db "
 shift
 # If usbcam add --device=/dev/video0:/dev/video0 ... Will improve to detect and add it auto from config.json
-docker run -d --name opendatacam --restart unless-stopped --gpus=all -p 8080:8080 -p 8090:8090 -p 8070:8070 $DOCKER_VOLUMES $@ opendatacam/opendatacam:OPENDATACAM_VERSION-nvidiadocker
+# TODO change back to normal opendatacam image
+# temporarily run with "sudo run.sh image-name"
+# docker run -d --name opendatacam --restart unless-stopped --gpus=all -p 8080:8080 -p 8090:8090 -p 8070:8070 $DOCKER_VOLUMES $@ opendatacam/opendatacam:OPENDATACAM_VERSION-nvidiadocker
+docker run -d --name opendatacam --restart unless-stopped --gpus=all -p 8080:8080 -p 8090:8090 -p 8070:8070 $DOCKER_VOLUMES $@
